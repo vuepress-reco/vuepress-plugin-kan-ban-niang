faaf5b209d3e4ad966a8d6665f9f3f2c3c01b2f5
LocTran <trungphuc016@Gmail.com>
2020-09-17 12:47:24 +0700

    Fix misc
    Change from `welocme` to `welcome`

--- bin/KanBanNiang.vue
+++ bin/KanBanNiang.vue
@@ -112,7 +112,7 @@ export default {
       this.messages.message = this.messages.close
     },
     resetMessage () {
-      this.messages.message = this.messages.welocme
+      this.messages.message = this.messages.welcome
     },
     goHome () {
       if (this.$route.path !== '/') {
